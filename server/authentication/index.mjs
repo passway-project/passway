6998a77ef3862af7cd67ebe67693e8a4d50fb6cb
Jeremy Kahn <me@jeremyckahn.com>
2024-06-22 16:45:02 -0500

    feat(auth): connect to db

--- server/authentication/index.mjs
+++ server/authentication/index.mjs
@@ -1,6 +1,18 @@
 import Koa from 'koa'
+import pg from 'pg'
+
+const dbContainerName = 'db'
+
+const dbClient = new pg.Client({
+  database: process.env.DB_NAME,
+  host: dbContainerName,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+})
 
 const app = new Koa()
+await dbClient.connect()
 
 app.use(async ctx => {
   ctx.body = 'Hello World'

75f4e5cff4740ea7b7612d6b4b883b5070479b39
Jeremy Kahn <me@jeremyckahn.com>
2024-06-24 08:59:54 -0500

    feat(auth): [wip] start scaffolding user endpoint

--- server/authentication/src/routes.ts
+++ server/authentication/src/routes.ts
@@ -24,4 +24,54 @@ export const initRoutes = () => {
       reply.send({ hello: 'world' })
     },
   })
+
+  // FIXME: Implement versioning: https://fastify.dev/docs/latest/Reference/Routes/#route-prefixing
+  app.put(
+    '/user',
+    {
+      schema: {
+        description: 'post some data',
+        tags: ['user', 'code'],
+        summary: 'Endpoint for creating/updating a user record',
+        params: {
+          type: 'object',
+          properties: {
+            id: {
+              type: 'string',
+              description: 'user id',
+            },
+          },
+        },
+        body: {
+          type: 'object',
+          properties: {
+            hello: { type: 'string' },
+            obj: {
+              type: 'object',
+              properties: {
+                some: { type: 'string' },
+              },
+            },
+          },
+        },
+        response: {
+          201: {
+            description: 'Successful response',
+            type: 'object',
+            properties: {
+              hello: { type: 'string' },
+            },
+          },
+          default: {
+            description: 'Default response',
+            type: 'object',
+            properties: {
+              foo: { type: 'string' },
+            },
+          },
+        },
+      },
+    },
+    (req, reply) => {}
+  )
 }

0f1000af2823af168c6270cde40b9b78f9b47cd3
Jeremy Kahn <me@jeremyckahn.com>
2024-06-24 21:35:43 -0500

    refactor(auth): decouple routes from plugin

--- server/authentication/src/index.ts
+++ server/authentication/src/index.ts
@@ -1,9 +1,9 @@
 import { app } from './app'
-import { initRoutes } from './routes'
+import { routes } from './routes'
 import { dbClient } from './db-client'
 
 dbClient.connect().then(async () => {
-  initRoutes()
+  app.register(routes, { prefix: '/api/v1' })
 
   try {
     await app.listen({ host: '0.0.0.0', port: 3000 })
--- server/authentication/src/routes.ts
+++ server/authentication/src/routes.ts
@@ -1,58 +1,52 @@
-import { app } from './app'
+import { FastifyPluginAsync } from 'fastify'
 
-const prefix = '/api/v1'
-export const initRoutes = () => {
-  app.register(
-    async (app, options, done) => {
-      app.put(
-        '/user',
-        {
-          schema: {
-            description: 'post some data',
-            tags: ['user'],
-            summary: 'Endpoint for creating/updating a user record',
-            params: {
+export const routes: FastifyPluginAsync = async app => {
+  app.put(
+    '/user',
+    {
+      schema: {
+        description: 'post some data',
+        tags: ['user'],
+        summary: 'Endpoint for creating/updating a user record',
+        params: {
+          type: 'object',
+          properties: {
+            id: {
+              type: 'string',
+              description: 'user id',
+            },
+          },
+        },
+        body: {
+          type: 'object',
+          properties: {
+            hello: { type: 'string' },
+            obj: {
               type: 'object',
               properties: {
-                id: {
-                  type: 'string',
-                  description: 'user id',
-                },
-              },
-            },
-            body: {
-              type: 'object',
-              properties: {
-                hello: { type: 'string' },
-                obj: {
-                  type: 'object',
-                  properties: {
-                    some: { type: 'string' },
-                  },
-                },
-              },
-            },
-            response: {
-              200: {
-                description: 'Successful response',
-                type: 'object',
-                properties: {
-                  hello: { type: 'string' },
-                },
-              },
-              default: {
-                description: 'Default response',
-                type: 'object',
-                properties: {
-                  foo: { type: 'string' },
-                },
+                some: { type: 'string' },
               },
             },
           },
         },
-        (req, reply) => {}
-      )
+        response: {
+          200: {
+            description: 'Successful response',
+            type: 'object',
+            properties: {
+              hello: { type: 'string' },
+            },
+          },
+          default: {
+            description: 'Default response',
+            type: 'object',
+            properties: {
+              foo: { type: 'string' },
+            },
+          },
+        },
+      },
     },
-    { prefix }
+    (req, reply) => {}
   )
 }

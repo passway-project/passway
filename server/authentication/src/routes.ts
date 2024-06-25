2b82ec6420d6dd19f7ce03158e9c6305f189f635
Jeremy Kahn <me@jeremyckahn.com>
2024-06-24 21:22:10 -0500

    feat(auth): prefix routes

--- server/authentication/src/routes.ts
+++ server/authentication/src/routes.ts
@@ -1,77 +1,58 @@
 import { app } from './app'
 
+const prefix = '/api/v1'
 export const initRoutes = () => {
-  app.route({
-    method: 'GET',
-    url: '/',
-    schema: {
-      // the response needs to be an object with an `hello` property of type 'string'
-      response: {
-        200: {
-          type: 'object',
-          properties: {
-            hello: { type: 'string' },
-          },
-        },
-      },
-    },
-    // this function is executed for every request before the handler is executed
-    preHandler: (request, reply, done) => {
-      // E.g. check authentication
-      done()
-    },
-    handler: (request, reply) => {
-      reply.send({ hello: 'world' })
-    },
-  })
-
-  // FIXME: Implement versioning: https://fastify.dev/docs/latest/Reference/Routes/#route-prefixing
-  app.put(
-    '/user',
-    {
-      schema: {
-        description: 'post some data',
-        tags: ['user', 'code'],
-        summary: 'Endpoint for creating/updating a user record',
-        params: {
-          type: 'object',
-          properties: {
-            id: {
-              type: 'string',
-              description: 'user id',
-            },
-          },
-        },
-        body: {
-          type: 'object',
-          properties: {
-            hello: { type: 'string' },
-            obj: {
+  app.register(
+    async (app, options, done) => {
+      app.put(
+        '/user',
+        {
+          schema: {
+            description: 'post some data',
+            tags: ['user'],
+            summary: 'Endpoint for creating/updating a user record',
+            params: {
               type: 'object',
               properties: {
-                some: { type: 'string' },
+                id: {
+                  type: 'string',
+                  description: 'user id',
+                },
+              },
+            },
+            body: {
+              type: 'object',
+              properties: {
+                hello: { type: 'string' },
+                obj: {
+                  type: 'object',
+                  properties: {
+                    some: { type: 'string' },
+                  },
+                },
+              },
+            },
+            response: {
+              200: {
+                description: 'Successful response',
+                type: 'object',
+                properties: {
+                  hello: { type: 'string' },
+                },
+              },
+              default: {
+                description: 'Default response',
+                type: 'object',
+                properties: {
+                  foo: { type: 'string' },
+                },
               },
             },
           },
         },
-        response: {
-          201: {
-            description: 'Successful response',
-            type: 'object',
-            properties: {
-              hello: { type: 'string' },
-            },
-          },
-          default: {
-            description: 'Default response',
-            type: 'object',
-            properties: {
-              foo: { type: 'string' },
-            },
-          },
-        },
-      },
+        (req, reply) => {}
+      )
     },
-    (req, reply) => {}
+    { prefix }
   )
 }

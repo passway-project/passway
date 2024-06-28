6e4fdb9f10536eb0d1d524f5a1f561f04e18ff5d
Jeremy Kahn <me@jeremyckahn.com>
2024-06-28 08:42:04 -0500

    test(auth): validate user endpoint success response

--- server/authentication/src/routes/v1/user/index.test.ts
+++ server/authentication/src/routes/v1/user/index.test.ts
@@ -1,4 +1,35 @@
-// FIXME: Add actual tests
-test('test runs', () => {
-  expect(1 + 1).toEqual(2)
+import fastify, { FastifyInstance } from 'fastify'
+import { buildApp } from '../../../app'
+import { API_ROOT } from '../../../constants'
+
+// FIXME: Move this to a setup file
+let app: FastifyInstance = fastify()
+
+beforeEach(async () => {
+  app = await buildApp()
+})
+
+afterEach(async () => {
+  app.close()
+})
+
+const endpointRoute = `/${API_ROOT}/v1/user`
+
+// FIXME: Silence logs for tests
+// FIXME: Mock the database
+
+describe(endpointRoute, () => {
+  test('creates a user', async () => {
+    const response = await app.inject({
+      method: 'PUT',
+      url: endpointRoute,
+      body: { id: 'foo' },
+    })
+
+    const bodyJson = await response.json()
+
+    expect(bodyJson).toEqual({ success: true })
+
+    // FIXME: Expect 200 response
+  })
 })

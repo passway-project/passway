b9055d01c54dcf8c2de08d77d25c5f5f88a6ab01
Jeremy Kahn <me@jeremyckahn.com>
2024-06-28 08:17:43 -0500

    refactor(auth): initialize app with factory pattern

--- server/authentication/src/app.ts
+++ server/authentication/src/app.ts
@@ -4,26 +4,25 @@ import swaggerUi from '@fastify/swagger-ui'
 import { SwaggerTheme, SwaggerThemeNameEnum } from 'swagger-themes'
 import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox'
 import { API_ROOT } from './constants'
+import * as routes from './routes/index'
 
 const theme = new SwaggerTheme()
 const content = theme.getBuffer(SwaggerThemeNameEnum.DARK)
 
-export const app = Fastify({
-  logger: {
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss Z',
-        ignore: 'pid,hostname',
+export const buildApp = async () => {
+  const app = Fastify({
+    logger: {
+      transport: {
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+          translateTime: 'HH:MM:ss Z',
+          ignore: 'pid,hostname',
+        },
       },
     },
-  },
-}).withTypeProvider<TypeBoxTypeProvider>()
+  }).withTypeProvider<TypeBoxTypeProvider>()
 
-// NOTE: Invoke async because top-level await isn't supported until this is
-// shipped: https://github.com/TypeStrong/ts-node/pull/2073
-;(async () => {
   await app.register(swagger)
 
   await app.register(swaggerUi, {
@@ -38,4 +37,10 @@ export const app = Fastify({
       css: [{ filename: 'theme.css', content: content }],
     },
   })
-})()
+
+  for (const route of Object.values(routes)) {
+    app.register(route, { prefix: `/${API_ROOT}/v1` })
+  }
+
+  return app
+}
--- server/authentication/src/index.ts
+++ server/authentication/src/index.ts
@@ -1,13 +1,9 @@
-import { app } from './app'
-import { API_ROOT } from './constants'
-import * as routes from './routes/index'
+import { buildApp } from './app'
 
 // NOTE: Invoke async because top-level await isn't supported until this is
 // shipped: https://github.com/TypeStrong/ts-node/pull/2073
 ;(async () => {
-  for (const route of Object.values(routes)) {
-    app.register(route, { prefix: `/${API_ROOT}/v1` })
-  }
+  const app = await buildApp()
 
   try {
     await app.listen({ host: '0.0.0.0', port: 3000 })

21e8a80999d629a7a76b3191880830abf03cd7e0
Jeremy Kahn <me@jeremyckahn.com>
2024-07-02 21:04:50 -0500

    test(auth): validate user updating

--- server/authentication/src/routes/v1/user/index.test.ts
+++ server/authentication/src/routes/v1/user/index.test.ts
@@ -1,5 +1,5 @@
 import fastify, { FastifyInstance } from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, User } from '@prisma/client'
 import { StatusCodes } from 'http-status-codes'
 import { mockDeep, DeepMockProxy } from 'jest-mock-extended'
 import { buildApp } from '../../../app'
@@ -52,6 +52,52 @@ describe(endpointRoute, () => {
       },
       update: {},
       where: {
+        passkeyId,
+      },
+    })
+  })
+
+  test('updates a user', async () => {
+    const now = Date.now()
+    const passkeyId = 'foo'
+    const preexistingUser: User = {
+      id: 0,
+      passkeyId,
+      createdAt: new Date(now),
+      updatedAt: new Date(now),
+    }
+
+    ;(
+      app.prisma as DeepMockProxy<PrismaClient>
+    ).user.findFirstOrThrow.mockResolvedValueOnce(preexistingUser)
+    ;(
+      app.prisma as DeepMockProxy<PrismaClient>
+    ).user.upsert.mockResolvedValueOnce({
+      id: 0,
+      passkeyId,
+      createdAt: new Date(now),
+      updatedAt: new Date(now + 1000),
+    })
+
+    const response = await app.inject({
+      method: 'PUT',
+      url: endpointRoute,
+      body: { id: passkeyId },
+    })
+
+    const bodyJson = await response.json()
+
+    expect(bodyJson).toEqual({ success: true })
+    expect(response.statusCode).toEqual(StatusCodes.OK)
+    expect(
+      (app.prisma as DeepMockProxy<PrismaClient>).user.upsert
+    ).toHaveBeenCalledWith({
+      create: {
+        passkeyId,
+      },
+      update: {},
+      where: {
+        id: preexistingUser.id,
         passkeyId,
       },
     })

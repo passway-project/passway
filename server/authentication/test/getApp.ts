cb1d65c5a149d778e10df3c669285d6670f4dd7a
Jeremy Kahn <me@jeremyckahn.com>
2024-07-04 11:09:57 -0500

    chore(auth): prevent redis connection in test

--- server/authentication/test/getApp.ts
+++ server/authentication/test/getApp.ts
@@ -4,19 +4,23 @@ import { buildApp } from '../src/app'
 import { mockDeep } from 'jest-mock-extended'
 import { PrismaClient } from '@prisma/client'
 
+jest.mock('@fastify/redis', () => ({
+  __esModule: true,
+  default: async () => {},
+}))
+
+jest.mock('ioredis')
+
 let app: FastifyInstance = fastify()
-let redis: FastifyRedis | null = null
 
 beforeAll(async () => {
   app = await buildApp({ logger: false })
   app.prisma = mockDeep<PrismaClient>()
-  redis = app.redis
   app.redis = mockDeep<FastifyRedis>()
 })
 
 afterAll(() => {
   app.close()
-  redis?.quit()
 })
 
 export const getApp = () => {

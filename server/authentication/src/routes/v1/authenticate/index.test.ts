c2c6ef158b1341e3688c95e6133edd99b81eb5ce
Jeremy Kahn <me@jeremyckahn.com>
2024-07-04 17:37:53 -0500

    test(auth): [wip] set up stub keypair

--- server/authentication/src/routes/v1/authenticate/index.test.ts
+++ server/authentication/src/routes/v1/authenticate/index.test.ts
@@ -1,4 +1,5 @@
-import { PrismaClient, User } from '@prisma/client'
+import { webcrypto } from 'node:crypto'
+import { PrismaClient } from '@prisma/client'
 import { StatusCodes } from 'http-status-codes'
 import { DeepMockProxy } from 'jest-mock-extended'
 import { getApp } from '../../../../test/getApp'
@@ -6,6 +7,31 @@ import { API_ROOT } from '../../../constants'
 import { routeName } from '.'
 
 const endpointRoute = `/${API_ROOT}/v1/${routeName}`
+
+let publicKey = ''
+let privateKey = ''
+
+beforeEach(async () => {
+  const keypair = await webcrypto.subtle.generateKey(
+    {
+      name: 'RSA-OAEP',
+      modulusLength: 2048,
+      publicExponent: new Uint8Array([1, 0, 1]),
+      hash: 'SHA-256',
+    },
+    true,
+    ['encrypt', 'decrypt']
+  )
+
+  const textDecoder = new TextDecoder()
+
+  publicKey = textDecoder.decode(
+    await webcrypto.subtle.exportKey('spki', keypair.publicKey)
+  )
+  privateKey = textDecoder.decode(
+    await webcrypto.subtle.exportKey('pkcs8', keypair.privateKey)
+  )
+})
 
 describe(endpointRoute, () => {
   test('handles nonexistent user lookup', async () => {

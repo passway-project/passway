a025f30c821b15522fc72ac1727c717890eabdd1
Jeremy Kahn <me@jeremyckahn.com>
2024-07-07 14:49:37 -0500

    feat(auth): [wip] add POC signature code

--- server/authentication/src/routes/v1/authenticate/index.test.ts
+++ server/authentication/src/routes/v1/authenticate/index.test.ts
@@ -1,11 +1,11 @@
-import { ECKeyPairOptions, webcrypto } from 'node:crypto'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, User } from '@prisma/client'
 import { StatusCodes } from 'http-status-codes'
 import { DeepMockProxy } from 'jest-mock-extended'
 import { getApp } from '../../../../test/getApp'
 import { API_ROOT } from '../../../constants'
 import { routeName } from '.'
 import { getKeypair, signatureKeyParams } from '../../../../test/getKeypair'
+import { webcrypto } from 'crypto'
 
 const endpointRoute = `/${API_ROOT}/v1/${routeName}`
 
@@ -13,6 +13,7 @@ const stubUserId = 0
 const stubUserPasskeySecret = 'abc123'
 let stubUserEncryptedKeysData = ''
 let stubUserPublicKeyData = ''
+let stubUserPrivateKeyData = ''
 
 const importKey = async (password: string) => {
   const encoder = new TextEncoder()
@@ -46,6 +47,7 @@ beforeAll(async () => {
   const signatureKeys = await getKeypair(signatureKeyParams)
 
   stubUserPublicKeyData = signatureKeys.publicKey
+  stubUserPrivateKeyData = signatureKeys.privateKey
 
   const keysString = JSON.stringify({
     encryptionKeys,
@@ -92,5 +94,85 @@ describe(endpointRoute, () => {
 
     expect(bodyJson).toEqual({ success: false })
     expect(response.statusCode).toEqual(StatusCodes.NOT_FOUND)
+  })
+
+  test('creates session for valid user authentication request', async () => {
+    const app = getApp()
+    const passkeyId = 'foo'
+    const now = Date.now()
+    const preexistingUser: User = {
+      id: stubUserId,
+      passkeyId,
+      encryptedKeys: stubUserEncryptedKeysData,
+      publicKey: stubUserPublicKeyData,
+      createdAt: new Date(now),
+      updatedAt: new Date(now),
+    }
+
+    const publicKeyBuffer = Buffer.from(stubUserPublicKeyData, 'base64')
+    const signaturePublicKeyBuffer = await webcrypto.subtle.importKey(
+      'spki',
+      publicKeyBuffer,
+      {
+        name: signatureKeyParams.algorithm.name,
+        namedCurve: 'P-256',
+        hash: 'SHA-256',
+      },
+      true,
+      ['verify']
+    )
+
+    const privateKeyBuffer = Buffer.from(stubUserPrivateKeyData, 'base64')
+    const signaturePrivateKeyBuffer = await webcrypto.subtle.importKey(
+      'pkcs8',
+      privateKeyBuffer,
+      {
+        name: signatureKeyParams.algorithm.name,
+        namedCurve: 'P-256',
+        hash: 'SHA-256',
+      },
+      true,
+      ['sign']
+    )
+
+    // FIXME: Verify that salt works as expected
+    const saltLength = 32
+    const rawData = 'Hello, World!'
+    const dataBuffer = new TextEncoder().encode(rawData)
+    const signature = await webcrypto.subtle.sign(
+      {
+        name: signatureKeyParams.algorithm.name,
+        hash: 'SHA-256',
+        saltLength,
+      },
+      signaturePrivateKeyBuffer,
+      dataBuffer
+    )
+
+    // FIXME: Move this to the route handler
+    const isValid = await webcrypto.subtle.verify(
+      {
+        name: signatureKeyParams.algorithm.name,
+        hash: 'SHA-256',
+        saltLength,
+      },
+      signaturePublicKeyBuffer,
+      signature,
+      dataBuffer
+    )
+
+    ;(
+      app.prisma as DeepMockProxy<PrismaClient>
+    ).user.findFirstOrThrow.mockResolvedValueOnce(preexistingUser)
+
+    const response = await app.inject({
+      method: 'POST',
+      url: endpointRoute,
+      body: { id: passkeyId },
+    })
+
+    const bodyJson = await response.json()
+
+    expect(bodyJson).toEqual({ success: true })
   })
 })

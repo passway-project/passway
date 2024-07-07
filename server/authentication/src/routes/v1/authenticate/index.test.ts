e4b11249a12d5e10b8dd77f1f0bc1c6bbf82d96a
Jeremy Kahn <me@jeremyckahn.com>
2024-07-07 11:45:52 -0500

    test(auth): [wip] add keypair setup

--- server/authentication/src/routes/v1/authenticate/index.test.ts
+++ server/authentication/src/routes/v1/authenticate/index.test.ts
@@ -5,8 +5,80 @@ import { DeepMockProxy } from 'jest-mock-extended'
 import { getApp } from '../../../../test/getApp'
 import { API_ROOT } from '../../../constants'
 import { routeName } from '.'
+import { getKeypair } from '../../../../test/getKeypair'
 
 const endpointRoute = `/${API_ROOT}/v1/${routeName}`
+
+const stubUserId = 0
+const stubUserPasskeySecret = 'abc123'
+let stubUserEncryptedKeysData = ''
+let stubUserPublicKeyData = ''
+
+const importKey = async (password: string) => {
+  const encoder = new TextEncoder()
+
+  return crypto.subtle.importKey(
+    'raw',
+    encoder.encode(password),
+    { name: 'PBKDF2' },
+    false,
+    ['deriveKey']
+  )
+}
+
+const deriveKey = async (keyMaterial: CryptoKey, salt: BufferSource) => {
+  return crypto.subtle.deriveKey(
+    {
+      name: 'PBKDF2',
+      salt,
+      iterations: 100000,
+      hash: 'SHA-256',
+    },
+    keyMaterial,
+    { name: 'AES-GCM', length: 256 },
+    false,
+    ['encrypt', 'decrypt']
+  )
+}
+
+beforeAll(async () => {
+  const keypair = await getKeypair({
+    algorithm: {
+      name: 'ECDSA',
+      namedCurve: 'P-256',
+      hash: { name: 'SHA-256' },
+    },
+    usage: ['sign', 'verify'],
+  })
+
+  const { publicKey, privateKey } = keypair
+  stubUserPublicKeyData = publicKey
+
+  const keysString = JSON.stringify({
+    publicKey,
+    privateKey,
+  })
+
+  const encoder = new TextEncoder()
+  const salt = crypto.getRandomValues(new Uint8Array(16))
+  const iv = crypto.getRandomValues(new Uint8Array(12))
+
+  const importedKey = await importKey(stubUserPasskeySecret)
+  const derivedKey = await deriveKey(importedKey, salt)
+
+  const encryptedKeys = await crypto.subtle.encrypt(
+    {
+      name: 'AES-GCM',
+      iv,
+    },
+    derivedKey,
+    encoder.encode(keysString)
+  )
+
+  const encryptedKeysString = Buffer.from(encryptedKeys).toString('base64')
+
+  stubUserEncryptedKeysData = encryptedKeysString
+})
 
 describe(endpointRoute, () => {
   test('handles nonexistent user lookup', async () => {

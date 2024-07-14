5629885b57bf49a08e8899b3ab8ea7c44fb3be10
Jeremy Kahn <me@jeremyckahn.com>
2024-07-14 14:06:14 -0500

    test(int): validate user creation

--- server/authentication/test/integration/login-logout.integration-test.ts
+++ server/authentication/test/integration/login-logout.integration-test.ts
@@ -1,5 +1,41 @@
+import { StatusCodes } from 'http-status-codes'
+import { buildApp } from '../../src/app'
+import { API_ROOT } from '../../src/constants'
+import { routeName as userRouteName } from '../../src/routes/v1/user'
+import { getKeypair } from '../getKeypair'
+import { getStubKeyData } from '../getStubKeyData'
+import { redisClient } from '../../src/cache'
+
+const stubUserPasskeySecret = 'abc123'
+
 describe('login and logout', () => {
-  xtest('user can log in and log out', () => {
-    // FIXME: Implement this
+  test('user can log in and log out', async () => {
+    const app = await buildApp()
+    const keypair = await getKeypair()
+    const stubUserPublicKeyData = btoa(keypair.publicKey)
+    const stubKeyData = await getStubKeyData(stubUserPasskeySecret)
+
+    const passkeyId = 'foo'
+
+    const response = await app.inject({
+      method: 'PUT',
+      url: `/${API_ROOT}/v1/${userRouteName}`,
+      body: {
+        id: passkeyId,
+        encryptedKeys: stubKeyData.encryptedKeys,
+        publicKey: stubUserPublicKeyData,
+      },
+    })
+
+    const bodyJson = await response.json()
+
+    expect(bodyJson).toEqual({ success: true })
+    expect(response.statusCode).toEqual(StatusCodes.CREATED)
+
+    // FIXME: Get session
+    // FIXME: Destroy session
+
+    await app.close()
+    redisClient.disconnect()
   })
 })

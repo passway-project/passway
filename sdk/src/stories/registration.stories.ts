ef8c323b3da23f4ac646b3a4e5d97af55a060b88
Jeremy Kahn <me@jeremyckahn.com>
2024-07-30 09:00:56 -0500

    feat(sdk): make registration options configurable via storybook

--- sdk/src/stories/registration.stories.ts
+++ sdk/src/stories/registration.stories.ts
@@ -1,4 +1,6 @@
-import { passwayClient } from '..'
+import type { Meta, StoryObj } from '@storybook/html'
+
+import { RegistrationConfig, passwayClient } from '..'
 
 class RegistrationStory extends HTMLElement {
   private client = passwayClient
@@ -15,9 +17,9 @@ class RegistrationStory extends HTMLElement {
     registrationButton.innerHTML = `<code>initiateRegistration()</code>`
     registrationButton.addEventListener('click', async () => {
       await this.client.initiateRegistration({
-        appName: 'Passway Demo',
-        userName: 'example-user',
-        userDisplayName: 'Example User',
+        appName: this.getAttribute('app-name') ?? '',
+        userName: this.getAttribute('user-name') ?? '',
+        userDisplayName: this.getAttribute('user-display-name') ?? '',
       })
     })
 
@@ -30,12 +32,30 @@ window.customElements.define('story-registration', RegistrationStory)
 export default {
   title: 'Registration',
   tags: ['autodocs'],
-  render: () => {
+  render: ({ appName, userDisplayName, userName }) => {
     return `
 <h1>Passway Registration</h1>
-<story-registration />
+<story-registration app-name="${appName}" user-display-name="${userDisplayName}" user-name="${userName}"/>
 `
   },
-}
+  args: {
+    appName: 'Passway Demo',
+    userName: 'example-user',
+    userDisplayName: 'Example User',
+  },
+  argTypes: {
+    appName: {
+      type: 'string',
+    },
+    userName: {
+      type: 'string',
+    },
+    userDisplayName: {
+      type: 'string',
+    },
+  },
+} satisfies Meta<RegistrationConfig>
 
-export const Primary = {}
+export const Primary: StoryObj<RegistrationConfig> = {
+  args: {},
+}

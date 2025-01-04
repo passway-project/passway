d094e76eedd1dc61a6e7b4b7c45836c3dd78e2b2
Jeremy Kahn <me@jeremyckahn.com>
2025-01-04 09:54:10 -0600

    refactor(api): consolidate DB migrations

--- server/api/prisma/migrations/20241222235155_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `contentName` to the `FileMetadata` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "FileMetadata" ADD COLUMN     "contentName" TEXT NOT NULL;
--- server/api/prisma/migrations/20241230165338_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `isEncrypted` on the `FileMetadata` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "FileMetadata" DROP COLUMN "isEncrypted";
